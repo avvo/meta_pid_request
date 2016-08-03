10c1f6642a2cc9b7bc488cce56f358bfd6ebc48e
Chris Wilhelm <cwilhelm@avvo.com>
2016-08-03 16:13:34 -0700

    Tests for primary interface

--- test/meta_pid_request_test.exs
+++ test/meta_pid_request_test.exs
@@ -1,5 +1,6 @@
 defmodule MetaPidRequestTest do
   use ExUnit.Case
+  alias MetaPidRequest.RequestMetadata
 
   doctest MetaPidRequest
 
@@ -7,5 +8,33 @@ defmodule MetaPidRequestTest do
     pid = MetaPidRequest.start_link()
 
     %{server: pid}
+  end
+
+  test "registers a request with a time for the passed pid" do
+    MetaPidRequest.register_request(self, "asdf")
+
+    {:ok, data} = MetaPidRequest.Registry.fetch_pid(self)
+
+    assert is_integer(data.start_time)
+    assert data.request_id == "asdf"
+    assert data.times == %{}
+  end
+
+  test "retrives a registered pid's data" do
+    MetaPidRequest.register_request(self, "asdf")
+    assert MetaPidRequest.Registry.fetch_pid(self) == MetaPidRequest.fetch_metadata(self)
+  end
+
+  test "adds a time to the specified service list for a pid" do
+    MetaPidRequest.register_request(self, "asdf")
+
+    MetaPidRequest.add_time(self, :service1, 1)
+    MetaPidRequest.add_time(self, :service1, 2)
+    MetaPidRequest.add_time(self, :service1, 3)
+    MetaPidRequest.add_time(self, :service2, 1)
+
+    {:ok, %RequestMetadata{times: times}} = MetaPidRequest.fetch_metadata(self)
+
+    assert times == %{service1: [3,2,1], service2: [1]}
   end
 end
