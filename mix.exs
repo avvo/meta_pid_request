d8bdc072800a9186368021e6e5ac357af36c880c
Donald Plummer <dplummer@avvo.com>
2017-07-26 13:14:45 -0700

    Metadata for release

--- mix.exs
+++ mix.exs
@@ -6,11 +6,13 @@ defmodule MetaPidRequest.Mixfile do
       app: :meta_pid_request,
       build_embedded: Mix.env == :prod,
       deps: deps(),
+      description: description(),
       dialyzer: [
         plt_add_deps: :transitive,
         plt_file: {:no_warn, ".local.plt"}
       ],
       elixir: "~> 1.4",
+      package: package(),
       start_permanent: Mix.env == :prod,
       version: "0.2.1"
     ]
@@ -30,6 +32,23 @@ defmodule MetaPidRequest.Mixfile do
 
       # NON-PRODUCTION DEPS
       {:dialyxir, "~> 0.5", only: [:dev, :test]}
+    ]
+  end
+
+  def description do
+    """
+    MetaPidRequest provides an OTP application for keeping track of meta data associated with requests.
+    """
+  end
+
+  defp package do
+    [
+      name: :meta_pid_request,
+      maintainers: ["Avvo, Inc", "Donald Plummer"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/avvo/meta_pid_request"
+      }
     ]
   end
 end
